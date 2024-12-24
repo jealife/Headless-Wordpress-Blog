bbee094f57e90efbc4c0ab70dab971fccfc702ae
Jealife <jeanguylanememiaghe@gmail.com>
2024-12-24 02:40:17 +0100

    Changes to be committed: 	new file:   apollo-client.js 	modified:   app/globals.css 	modified:   app/layout.js 	modified:   app/page.js 	new file:   app/posts/[slug]/page.jsx 	new file:   app/posts/page.jsx 	new file:   components/ApolloWrapper.js 	modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   queries.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['pindiofficiel.com'], // Ajoute ton domaine ici
+    },
+};
 
 export default nextConfig;
